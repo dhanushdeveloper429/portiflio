ce47c38207c8e446fcb4c288b68df57f9308211b
katuridhanushkasrinivas <dhanush>
2020-08-09 02:50:46 -0400

    Add like badge count  and mat divider

--- profile/src/app/app.module.ts
+++ profile/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {MatBadgeModule} from '@angular/material/badge';
 import {MatChipsModule} from '@angular/material/chips';
@@ -46,7 +47,6 @@ import {MatTableModule} from '@angular/material/table';
     MatSliderModule,
     AppRoutingModule,
     FormsModule,
-
     BrowserAnimationsModule,
     MatExpansionModule,
     MatPaginatorModule,
